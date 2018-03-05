1f40ac59bb546e3eeb72819022268d7bf1206153
Damini <damini12605sharma@gmail.com>
2018-03-05 20:34:09 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FormsModule  } from  '@angular/forms';
+import { HttpModule } from  '@angular/http';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule} from 'angularfire2/database';
+
+export const firebaseConfig =
+{
+  apiKey: "AIzaSyBreQPny7N1xcLlxdR3TIkoKSzgXcBu0MY",
+  authDomain: "chatapp-279af.firebaseapp.com",
+  databaseURL: "https://chatapp-279af.firebaseio.com",
+  projectId: "chatapp-279af",
+  storageBucket: "chatapp-279af.appspot.com",
+  messagingSenderId: "735533697149"
+};
+
 
 
 @NgModule({
@@ -10,7 +26,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
